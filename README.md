ae1695958a6ba2c62f0eeadea81cd4be089f49d7
Neonidian <neonidian@gmail.com>
2020-04-11 22:49:21 +0200

    Remove dependency on geckodriver executable

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# selenium-javascript
+# Selenium Using Javascript
 A sample javascript project for showcasing Selenium to automate browser testing
+
+## Pre-requisites
+* Firefox
+* npm - Node Package Manager
+
+## Run
+In the commandline, enter `npm test` to run the tests 
--- test/test.js
+++ test/test.js
@@ -1,24 +1,20 @@
 const chai = require('chai')
-const { Builder, By, Key, until } = require('selenium-webdriver');
-const { ServiceBuilder } = require('selenium-webdriver/firefox')
+const {Builder, By, Key, until} = require('selenium-webdriver');
+require('geckodriver')
 
 describe('request', function () {
     it('should start a browser', function () {
         (async function example() {
-            const serviceBuilder = new ServiceBuilder(`${__dirname}/resources/geckodriver.exe`);
-            let driver = await new Builder().forBrowser('firefox').setFirefoxService(serviceBuilder).build();
+            let driver = await new Builder().forBrowser('firefox').build();
             try {
                 await driver.get('http://www.google.com/ncr');
                 await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
                 await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-            }
-            catch (error) {
+            } catch (error) {
                 console.error(`Error: ${error}`)
-            }
-            finally {
+            } finally {
                 await driver.quit();
             }
         })();
     });
 });
-
