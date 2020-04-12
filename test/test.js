8e824f6b696415b5f5047adf30a481013fae908c
Neonidian <neonidian@gmail.com>
2020-04-12 17:46:49 +0200

    Perform Google search for English language

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ describe('request', function () {
         (async function example() {
             let driver = await new Builder().forBrowser('firefox').build();
             try {
-                await driver.get('http://www.google.com/ncr');
+                await driver.get('http://www.google.com/en');
                 await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
                 await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
             } catch (error) {
