ff0512995445abc3e0b418da9cb348ecdf8e59e6
Neonidian <neonidian@gmail.com>
2020-04-13 20:30:37 +0200

    Use arrow function

--- test/translateTest.js
+++ test/translateTest.js
@@ -3,8 +3,8 @@ const expect = require('chai').expect;
 const BrowserFactory = require('./selenium/BrowserFactory');
 const HomePage = require('./pages/HomePage');
 
-describe('Google Translate', function() {
-    it('should translate Swedish to English', async function () {
+describe('Google Translate', () => {
+    it('should translate Swedish to English', async () => {
         let driver = await new BrowserFactory().startBrowser();
         let homepage = new HomePage(driver);
 
